5aa2ca1dd31cb7092282c73fefafb6c89d931af6
annieqc <90060228+annieqc@users.noreply.github.com>
2021-09-03 13:16:03 -0400

    Add files via upload

---
+++ Hello1.java
@@ -0,0 +1,22 @@
+/******************************************************
+* Java Program Name
+*******************************************************
+* Annie Choudhry
+* 09/01/2021
+* CMSC 255 41688
+******************************************************/
+
+public class Hello1 {
+    public static void main(String[] args) {
+
+	System.out.println("<Annie Choudhry>");
+        System.out.println("<Java Program Name - Lab 1>");
+	System.out.println("<Program description - Lab 1 content>");
+	System.out.println("<09/01/2021>");
+	System.out.println("<CMSC 255 #41688>");
+	System.out.println();
+    }
+}
+
+
+
